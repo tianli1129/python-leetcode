fef5ed9bab99abf61117e61ca372a3f88f881c6a
Tina <liutianli2011@gmail.com>
2024-07-21 11:40:57 +0800

    valid parentheses

---
+++ Array/ValidParentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mappings = {")":"(", "]":"[","}":"{"}
+        for char in s:
+            if char in mappings.values():
+                stack.append(char)
+            else:
+                if not stack or stack.pop()!=mappings.get(char):
+                    return False
+        return not stack
+
+print(Solution().isValid(s="([])"))
